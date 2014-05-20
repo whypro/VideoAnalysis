8577ecc606415d854970774478c24165eb3d0499
whypro <whypro@live.cn>
2014-05-21 00:59:28 +0800

    完善运动目标检测； 增加注释。

--- video_analysis.cpp
+++ video_analysis.cpp
@@ -7,6 +7,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/video/tracking.hpp>
 #include <iostream>
+#include <cmath>
 
 using std::cout;
 using std::endl;
@@ -16,63 +17,75 @@ VideoAnalysis::VideoAnalysis(QWidget *parent, Qt::WFlags flags)
 	: QMainWindow(parent, flags)
 {
 	ui.setupUi(this);
+	status_label = new QLabel();
+
 	// 初始化成员变量
 	timer = new QTimer(this);	// 初始化计时器
-	is_detecting = false;
+	is_detecting = false;		// 初始化检测标志位
 	timer->setInterval(1000/FPS);	// 设置计时器间隔
 
-	init_signal();
-	init_ui();
+	init_signal();	// 初始化Qt信号槽
+	init_ui();		// 初始化界面控件
 }
 
 void VideoAnalysis::init_ui() {
+	// 将 Action 添加到菜单栏，初始化状态栏
 	ui.tool_bar->addAction(ui.open_video_action);
 	ui.tool_bar->addSeparator();
 	ui.tool_bar->addAction(ui.detect_action);
+	ui.status_bar->addWidget(status_label);
+}
+
+void VideoAnalysis::destroy_ui() {
+	delete status_label;
 }
 
 void VideoAnalysis::init_signal() {
-	connect(ui.open_video_action, SIGNAL(triggered()), this, SLOT(openVideo()));	// 将菜单动作信号与函数槽相连接
+	connect(ui.open_video_action, SIGNAL(triggered()), this, SLOT(openVideo()));	// 将菜单动作信号与打开文件函数槽相连接
 	connect(timer, SIGNAL(timeout()), this, SLOT(display()));	// 将时钟信号与函数槽相连接	
-	connect(ui.detect_action, SIGNAL(triggered()), this, SLOT(start_detect()));
+	connect(ui.detect_action, SIGNAL(triggered()), this, SLOT(start_detect()));		// 将菜单动作与检测函数槽相连接
 }
 
 VideoAnalysis::~VideoAnalysis()
 {
 	delete timer;
 	timer = NULL;
+	destroy_ui();
 }
 
 void VideoAnalysis::openVideo() {
-	QString fileName = QFileDialog::getOpenFileName(this, "打开视频", ".", "Video (*.avi)");
+	QString fileName = QFileDialog::getOpenFileName(this, "打开视频", ".", "Video (*.avi)");	// 弹出选择文件对话框
 	capture.open(fileName.toStdString());	// 打开视频文件
 	assert(capture.isOpened());
 	timer->start();		// 计时器开启
 }
 
 void VideoAnalysis::display() {
-	capture >> frame;
-	if (frame.empty()) {
+	capture >> frame;	// 当视频文件的帧输入 frame 成员变量
+	if (frame.empty()) {	// 如果文件结束，停止
 		timer->stop();
 		return;
 	}
-
-	if (is_detecting) {
-		frame = detect();
+	Mat vis = frame;
+	if (is_detecting) {		// 如果监控标志位开启，则执行监控函数
+		vis = detect();
 	}
-	QImage image = QImage((const unsigned char *)(frame.data), frame.cols, frame.rows, QImage::Format_RGB888).rgbSwapped();
+	
+	// 将帧显示到界面上
+	QImage image = QImage((const unsigned char *)(vis.data), vis.cols, vis.rows, QImage::Format_RGB888).rgbSwapped();
 	ui.screen->setPixmap(QPixmap::fromImage(image));
 
 }
 
-
-
 void VideoAnalysis::start_detect() {
+	
 	if (ui.detect_action->isChecked()) {
+		// 点击检测按钮后，开始检测，则初始化历史帧和运动记录矩阵
 		prev_frame = frame.clone();
 		motion_history = Mat::zeros(frame.rows, frame.cols, CV_32F);
 		is_detecting = true;
 	} else {
+		// 停止检测
 		is_detecting = false;
 	}
 	
@@ -87,19 +100,33 @@ Mat VideoAnalysis::detect() {
 	Mat frame_diff, gray_diff, motion_mask;
 	absdiff(frame, prev_frame, frame_diff);		// 求两帧之差差
 	cvtColor(frame_diff, gray_diff, COLOR_BGR2GRAY);	// 灰度化
-	double thrs = 32;
+	double thrs = 32;									// 设置阈值
 	threshold(gray_diff, motion_mask, thrs, 1, THRESH_BINARY);	// 二值化
-	double timestamp = clock() / 1000;
-
-	updateMotionHistory(motion_mask, motion_history, timestamp, MHI_DURATION);
+	double timestamp = float(clock()) / 1000;				// 得到当前时间戳
+	updateMotionHistory(motion_mask, motion_history, timestamp, MHI_DURATION);	// 更新运动历史矩阵
 	Mat mg_mask, mg_orient;
-	calcMotionGradient(motion_history, mg_mask, mg_orient, MAX_TIME_DELTA, MIN_TIME_DELTA, 5);
+	calcMotionGradient(motion_history, mg_mask, mg_orient, MAX_TIME_DELTA, MIN_TIME_DELTA, 5);	// 计算运动历史图像的梯度方向
 	Mat seg_mask;
 	std::vector<Rect> seg_bounds;
-	segmentMotion(motion_history, seg_mask, seg_bounds, timestamp, MAX_TIME_DELTA);
-
+	segmentMotion(motion_history, seg_mask, seg_bounds, timestamp, MAX_TIME_DELTA);		// 将整个运动分割为独立的运动部分
+	status_label->setText(QString::number(timestamp));
 	Mat vis;
-	vis = frame.clone();
+	switch(ui.output_dial->value()) {
+	case 0:
+		vis = frame.clone();		// 正常显示
+		break;
+	case 1:
+		vis = frame_diff.clone();	// 帧间差分显示
+		break;
+//  	case 2:
+//  		threshold((motion_history - (timestamp - MHI_DURATION)) / MHI_DURATION, vis, 0, 255, THRESH_BINARY);	// 二值化
+//  		cvtColor(vis, vis, COLOR_GRAY2BGR);
+//  		break;
+
+	default:
+		vis = frame.clone();
+		break;
+	}
 	int height = frame.rows;
 	int width = frame.cols;
 	vector<Rect>::iterator it = seg_bounds.begin();
@@ -115,30 +142,33 @@ Mat VideoAnalysis::detect() {
 			continue;
 		}
 
-		Mat silh_roi = motion_mask(Range(y, y+rh), Range(x, x+rw));
-		Mat orient_roi = mg_orient(Range(y, y+rh), Range(x, x+rw));
-		Mat mask_roi = mg_mask(Range(y, y+rh), Range(x, x+rw));
-		Mat mhi_roi = motion_history(Range(y, y+rh), Range(x, x+rw));
+		
+		Mat silh_roi = motion_mask(Range(y, y+rh), Range(x, x+rw));		// 获取图像ROI
+		Mat orient_roi = mg_orient(Range(y, y+rh), Range(x, x+rw));		// 获取图像ROI
+		Mat mask_roi = mg_mask(Range(y, y+rh), Range(x, x+rw));			// 获取图像ROI
+		Mat mhi_roi = motion_history(Range(y, y+rh), Range(x, x+rw));	// 获取图像ROI
 		if (norm(silh_roi, NORM_L1) < area*0.05) {
 			continue;
 		}
-		double angle = calcGlobalOrientation(orient_roi, mask_roi, mhi_roi, timestamp, MHI_DURATION);
-		Scalar color(0, 0, 255);	// BGR
-		if (i == 0) {
-			color[0] = 255;
-			color[2] = 0;
+		double angle = calcGlobalOrientation(orient_roi, mask_roi, mhi_roi, timestamp, MHI_DURATION);	// 计算每个运动部件的运动方向
+		Scalar color(255, 0, 0);	// BGR，默认为蓝色
+		if (i == 0) {	// 如果运动范围大于阈值，设置为红色
+			color[0] = 0;
+			color[2] = 255;
 		}
-		draw_motion_comp(vis, *it, angle, color);
+		draw_motion_comp(vis, *it, angle, color);	// 在该帧上画出区域
 	}
-	prev_frame = frame.clone();
+	prev_frame = frame.clone();		// 保存历史帧
 	return vis;
 }
 
 void VideoAnalysis::draw_motion_comp(Mat &vis, Rect rect, double angle, Scalar color) {
-	rectangle(vis, rect, color, 3);
+	// 绘制目标区域，和运动方向
+	rectangle(vis, rect, Scalar(0, 255, 0), 3);
 	int r = min(rect.width/2, rect.height/2);
-    int cx = rect.x + rect.width / 2;
-	int cy = rect.y + rect.height / 2;
-    // double angle = angle*3.14/180;
-	circle(vis, Point(cx, cy), r, color, 3);
+	Point center(rect.x + rect.width / 2, rect.y + rect.height / 2);
+    angle = angle*M_PI/180;
+	circle(vis, center, r, color, 3);
+	line(vis, center, Point(int(center.x+cos(angle)*r), int(center.y+sin(angle)*r)), color, 3);
+
 }
--- video_analysis.h
+++ video_analysis.h
@@ -18,26 +18,29 @@ public:
 	~VideoAnalysis();
 
 private:
-	Ui::VideoAnalysisClass ui;
-	void init_signal();
-	void init_ui();
+	Ui::VideoAnalysisClass ui;		// Qt UI
+	void init_signal();				// 初始化信号槽
+	void init_ui();					// 初始化空间
+	void destroy_ui();				// 销毁控件
 	
-	QTimer *timer;
-	static const int FPS = 24;
-	Mat frame;
-	Mat prev_frame;
-	Mat motion_history;
-	VideoCapture capture;
-	bool is_detecting;
+	QTimer *timer;					// 计时器
+	static const int FPS = 24;		// 每秒的帧数
+	Mat frame;						// 当前帧
+	Mat prev_frame;					// 上一帧
+	Mat motion_history;				// 运动历史
+	VideoCapture capture;			// 视频对象
+	bool is_detecting;				// 检测标识位
 
-	Mat detect();
-	void draw_motion_comp(Mat &vis, Rect rect, double angle, Scalar color);
+	Mat detect();					// 目标检测函数
+	void draw_motion_comp(Mat &vis, Rect rect, double angle, Scalar color);		// 区域绘制函数
+
+	QLabel* status_label;			// 状态栏的Label
 
 
 private slots:
-	void openVideo();
-	void display();
-	void start_detect();
+	void openVideo();				// 打开视频函数
+	void display();					// 显示帧函数
+	void start_detect();			// 开始检测函数
 };
 
 #endif // VIDEO_ANALYSIS_H
